f1ba6c14754b6a32d848b9b2b7c48aff3787c39d
shangdi1122 <1399070052@qq.com>
2017-08-04 11:51:55 +0800

    项目修改

---
+++ src/main/java/cn/bdqn/tangcco/xiangmu/XiangMu.java
@@ -0,0 +1,15 @@
+package cn.bdqn.tangcco.xiangmu;
+
+
+import org.junit.Test;
+
+/**
+ * Created by THINKPAD on 2017/8/4.
+ */
+public class XiangMu {
+@Test
+    public void Thanks() {
+    System.out.println("为什么不行呢");
+}
+
+}
